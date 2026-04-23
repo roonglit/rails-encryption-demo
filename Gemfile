89744d7b1413fb9c49f5844c7ec8597e615d8102
Roonglit <roonglit@gmail.com>
2026-04-23 11:29:10 +0700

    login with google

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem "image_processing", "~> 1.2"
 
 gem "devise"
 gem "tailwindcss-rails"
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    User.where(email: data["email"]).first_or_create do |user|
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
